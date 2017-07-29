07828682ef650d412d4f23d2128fd58b9de91002
dreac0nic <spenser.m.bray@gmail.com>
2017-07-28 23:28:16 -0700

    Raycast damage from a click

--- Assets/scripts/debug/health/RaycastDamageOnClick.cs
+++ Assets/scripts/debug/health/RaycastDamageOnClick.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RaycastDamageOnClick : MonoBehaviour {
+[RequireComponent(typeof(Camera))]
+public class RaycastDamageOnClick : MonoBehaviour
+{
+  public float Damage = 5.0f;
+  public float Distance = 100.0f;
+  public string ButtonName = "Fire1";
+  
+  private Camera m_Camera;
+  
+  public void Awake() {
+    m_Camera = GetComponent<Camera>();
+  }
+  
+  public void Update() {
+    if(Input.GetButtonDown(ButtonName)) { // button pushed
+      RaycastHit hit;
+      Ray cast_ray = m_Camera.ScreenPointToRay(Input.mousePosition);
 
-	// Use this for initialization
-	void Start () {
-		
+      if(Physics.Raycast(cast_ray, out hit, Distance)) {
+	Living hit_living = hit.collider.GetComponentInParent<Living>();
+
+	if(hit_living) {
+	  hit_living.TakeDamage(Damage);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+      }
+    }
+  }
 }
